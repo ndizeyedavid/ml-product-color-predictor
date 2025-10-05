37fca1531e6139990eb65a01f396c5ef7a0a7c1d
ndizeyedavid <davidndizeye101@gmail.com>
2025-10-05 10:49:06 +0200

    Changed Header name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -79,7 +79,7 @@ export default function App() {
       <header className="bg-blue-900 text-white py-6 mb-8 shadow-lg">
         <div className="max-w-7xl mx-auto px-4">
           <h1 className="text-4xl font-bold text-center">
-            Real-Time Color Prediction
+            Real-Time Color Classification
           </h1>
         </div>
       </header>
